2d935978f1b55c758dfbbed8c8a3e170d7ce1450
code-arif <arifulislam6460@gmail.com>
2026-03-14 14:09:14 +0600

    change folder name

--- routes/backend.php
+++ routes/backend.php
@@ -221,7 +221,7 @@ Route::prefix('cms')->name('cms.')->group(function () {
     // Apartment section update
     Route::post('/home/apartment/update', [ApartmentController::class, 'update'])->name('home.apartment.section.update');
 
-    // Upload gallery image
+    // Upload gallery
     Route::post('/gallery/update', [GalleryController::class, 'store'])->name('gallery.section.update');
     Route::delete('/gallery/item/delete/{id}', [GalleryController::class, 'destroy'])->name('gallery.item.delete');
 
