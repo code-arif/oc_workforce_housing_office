02e172601d64496d8c38a3eb1b43bf074eb5cf0b
code-arif <arifulislam6460@gmail.com>
2026-03-14 12:49:35 +0600

    new_update_s

--- resources/views/backend/layouts/cms/Properties/properties-one-banner.blade.php
+++ resources/views/backend/layouts/cms/Properties/properties-one-banner.blade.php
@@ -25,7 +25,7 @@
                         <div class="invalid-feedback"></div>
                     </div>
 
-                    {{-- Image --}}
+
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-group mb-3">
