2f105c2de8b167b469bae76a23f63a801b3f5071
code-arif <arifulislam6460@gmail.com>
2026-05-26 10:35:21 +0600

    feat: create rent report layout with filter and export functionality

--- resources/views/backend/layouts/reports/rent-report.blade.php
+++ resources/views/backend/layouts/reports/rent-report.blade.php
@@ -150,8 +150,8 @@
                                         <select class="form-select form-select-sm select3" id="tenantFilter">
                                             <option value="">Select Tenant</option>
                                             @foreach ($tenants as $tenant)
-                                                <option value="{{ $tenant->id }}">{{ $tenant->profile->first_name }}
-                                                    {{ $tenant->profile->last_name }}
+                                                <option value="{{ $tenant->id }}">{{ $tenant?->profile?->first_name }}
+                                                    {{ $tenant?->profile?->last_name }}
                                                 </option>
                                             @endforeach
                                         </select>
