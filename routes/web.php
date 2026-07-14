6020d2be9547741a1b08302f9ecd31c93d11ebec
code-arif <arifulislam6460@gmail.com>
2026-07-14 15:16:25 +0600

    feat: add welcome route to display the welcome view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 use App\Http\Controllers\Api\Tenants\TenantPaymentController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 // Handle webhook
 Route::post('/stripe/webhook', [TenantPaymentController::class, 'handleWebhook']);
 
