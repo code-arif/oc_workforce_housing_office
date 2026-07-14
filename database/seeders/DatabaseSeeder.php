6fd0022c0f0efc8864499e262e8fb4318b02b83c
code-arif <arifulislam6460@gmail.com>
2026-07-14 11:11:01 +0600

    chore: reinitialize DatabaseSeeder with empty call array

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,21 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            RolePermissionSeeder::class,
-            UserSeeder::class,
-            SettingSeeder::class,
-            PropertyTypeSeeder::class,
-            PropertySeeder::class,
-            UnitSeeder::class,
-            RoomSeeder::class,
-            BedSeeder::class,
-            TenantSeeder::class,
-            TenantProfileSeeder::class,
-            SeasonSeeder::class,
-            // LeaseSeeder::class,
-            // LeaseAssignmentSeeder::class,
-            AmenitySeeder::class,
-        ]);
+        $this->call([]);
     }
 }
